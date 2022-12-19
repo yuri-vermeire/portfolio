4d6be300e1efd29c325c11a981f478164a9abfb0
Yuri <yurivermeire@gmail.com>
2022-12-19 14:03:58 +0100

    Add margin auto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'about',
+    path: '',
     loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
   }, 
   {
